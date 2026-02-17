2d13e33d1edde59aaea30515482526f06050de6b
Sebas Monje <sebasmonje12@gmail.com>
2026-02-17 11:09:18 -0300

    actualización semana3

--- scripts_2026_1C/Primera_Parte/semana3.py
+++ scripts_2026_1C/Primera_Parte/semana3.py
@@ -1,93 +1,151 @@
-# Clase 2 - semana 3
-# radio = 5
-# area = pi * radio ** 2
-# 78.54 --> print(area)
+# # Clase 2 - semana 3
+# # radio = 5
+# # area = pi * radio ** 2
+# # 78.54 --> print(area)
 
-entero = 10 # 10 -10 0
-real = 3.14 # -0.5 0.0 0.5
-texto = "programación" # "N palabras" "1234%^&$#" "True y falso"
-booleano = True # True False
+# algo = 10 # representación de un entero.
+# algo = 10.2 
 
-print(type(10))
-print(type(3.14))
-print(type("programación"))
-print(type(True))
+# entero = 10 # 10 -10 0
+# real = 3.14 # -0.5 0.0 0.5
+# texto = "programación algo mas, shevbksv 123e13 #$%^" # "N palabras" "1234%^&$#" "True y falso"
+# booleano = True # True False
+# print(type(entero))
+# print(type(10.0))
+# print(type(3.14))
+# print(type("programación"))
+# print(type(True))
+# asdf1_ = 10 
+# """
+# Palabras reservadas
+# True = 10
+# False
+# if 
+# else
+#     for
+#     while\
+#     or 
+#     and
+#     elif
+#     e
+# """
 
-print(entero)
-print(real)
-print(texto)
-print(booleano)
+# print(entero)
+# print(real)
+# print(texto)
+# print(booleano)
 # print(type(entero))
 # print(type(real))
 # print(type(texto))
 # print(type(booleano))
-"""
-Una variable es un espacio en memoria que se reserva 
-para almacenar un valor.
-El tipo de dato de una variable determina el tipo de 
-valor que puede almacenar y las operaciones que se 
-pueden realizar con ella.
-"""
-edad = 20
-print(edad)
-
-edad = 21
-print(edad)
-
-
-a = 10
-b = 5
-
-resultado = a + b # suma
-
-print(resultado)
-resultado = a - b # resta
-print(resultado)
-resultado = a * b # multiplicación
-print(resultado)
-resultado = a / b # división
-print(resultado)
-resultado = a // b # división entera
-print(resultado)
-resultado = a % b # módulo o resto de la división
-print(resultado) 
-resultado = a ** b # potencia
-print(resultado)
-resultado = a + b * 2
-print(resultado) # 10 + 5 * 2 = 10 + 10 = 20
-resultado = (a + b) * 2
-print(resultado) # (10 + 5) * 2 = 15 * 2 = 30
-
-
-"""
-Entrada de datos: input()
-Salida de datos: print()
-"""
-nombre = input("Ingrese su nombre: ")
-
-print(nombre)
-edad = input("Ingrese su edad: ")
-print(type(edad))
 
 
 
-edad = input("Ingrese su edad: ")
 
-edad = int(edad)
 
-print(type(edad))
 
-###############################33
 
-numero1 = input("Ingrese numero: ")
-numero2 = input("Ingrese numero: ")
 
-numero1 = int(numero1)
-numero2 = int(numero2)
+# """
+# Una variable es un espacio en memoria que se reserva 
+# para almacenar un valor.
+# El tipo de dato de una variable determina el tipo de 
+# valor que puede almacenar y las operaciones que se 
+# pueden realizar con ella.
+# """
 
-suma = numero1 + numero2
+# string = "hola "
+# string1 = "soy "
+# string2 =" Sebas"
+# print(string + string1 + string2)
+# print(string * 3, 5 + 5, 3.3 ** 2)
 
-print(suma)
+
+
+
+# edad = 20
+# print(edad)
+
+# edad = 21
+# print(edad)
+
+
+# a = 10
+# b = 5
+# resultado = a + b # suma
+# print(resultado)
+# resultado = a - b # resta
+# print(resultado)
+# resultado = a * b # multiplicación
+# print(resultado)
+# resultado = b / a # división
+# print(resultado)
+# resultado = b // a # división entera
+# res = int(b/a)
+# print(resultado, res)
+# resultado = b % a # módulo o resto de la división
+# res = 20 % 2
+# print("Módulo de 20 con 2 es ", res)
+# print(resultado) 
+# resultado = a ** b # potencia
+# print(resultado)
+# resultado = a + b * 2
+# print("a+b*2",resultado) # 10 + 5 * 2 = 10 + 10 = 20
+# resultado = (a + b) * 2
+# print(resultado) # (10 + 5) * 2 = 15 * 2 = 30
+
+
+
+
+
+
+
+
+# """
+# Entrada de datos: input()
+# Salida de datos: print()
+# """
+# nombre = input("Ingrese su nombre: ")
+
+# print(nombre)
+# edad = int(input("Ingrese su edad: "))
+# print(type(edad))
+# valor = float(input("ingrese un float: "))
+# print(valor, type(valor))
+
+
+
+
+
+
+
+
+
+# edad = input("Ingrese su edad: ")
+
+# edad = int(edad)
+
+# print(type(edad))
+
+
+
+
+
+
+
+
+# ###############################33
+
+# numero1 = input("Ingrese numero: ")
+# numero2 = input("Ingrese numero: ")
+
+# numero1 = int(numero1)
+# numero2 = float(numero2)
+
+# suma = int(numero1 + numero2)
+
+# suma = int(suma)
+# print(int(suma))
 
 
 print("10" + "5")
@@ -96,47 +154,48 @@ print("10" + "5")
 año = input("Ingrese su año de nacimiento: ")
 año = int(año)
 edad = 2026 - año
-print("Hola", nombre)
+print("Hola", "nombre")
 print("Su edad es", edad)
 
 
 
-"""
-Escriba un programa que solicite al usuario el 
-el largo y ancho de un rectángulo y mostrar el área.
-"""
+# """
+# Escriba un programa que solicite al usuario el 
+# el largo y ancho de un rectángulo y mostrar el área.
+# """
 
 
-"""
-Un vehículo recorre una cierta distancia y consume una determinada 
-cantidad de combustible.
+# """
+# Un vehículo recorre una cierta distancia y consume una determinada 
+# cantidad de combustible.
 
-Escriba un programa en Python que:
-Solicite al usuario su nombre, la distancia recorrida en kilómetros (float) y la 
-cantidad de combustible consumido en litros (int)
+# Escriba un programa en Python que:
+# Solicite al usuario su nombre, la distancia recorrida en 
+# kilómetros (float) y la cantidad de combustible consumido 
+# en litros (int)
 
-Calcule el consumo de combustible, definido como:
-consumo=distancia/combustible
+# Calcule el consumo de combustible, definido como:
+# consumo=distancia/combustible
 
-Muestre como salida el nombre del usuario y el consumo calculado
+# Muestre como salida el nombre del usuario y el consumo calculado
 
-Se espera algo como:
-Ingrese su nombre: Sebas
-Ingrese la distancia recorrida (km): 150
-Ingrese el combustible consumido (litros): 10
+# Se espera algo como:
+# Ingrese su nombre: Sebas
+# Ingrese la distancia recorrida (km): 150
+# Ingrese el combustible consumido (litros): 10
 
-Usuario: Sebas
-Consumo: 15.0 km/l
-"""
+# Usuario: Sebas
+# Consumo: 15.0 km/l
+# """
 
 
-"""
-El IMC se calcula como:
+# """
+# El IMC se calcula como:
 
-IMC=peso/altura**2
+# IMC=peso/altura**2
 
-Solicitar:
-nombre
-peso en kg
-altura en metros
-"""
+# Solicitar:
+# nombre
+# peso en kg
+# altura en metros
+# """
